6b49df8735f87262c5d7fc036e19e888d540bd24
souravpanigrahi <souravpanigrahi2021@gmail.com>
2023-07-17 16:35:09 +0530

    Added getAll and Post

---
+++ src/controllers/airplane-controllers.js
@@ -0,0 +1,40 @@
+const { StatusCodes } = require("http-status-codes");
+
+const { AirplaneService } = require("../services");
+const { SuccessResponse, ErrorResponse } = require("../utils/common");
+
+/**
+ * POST : /airplanes
+ * req-body {modelNumber: 'airbus320', capacity: 200}
+ */
+async function createAirplane(req, res) {
+  //console.log(req);
+  try {
+    const airplane = await AirplaneService.createAirplane({
+      modelNumber: req.body.modelNumber,
+      capacity: req.body.capacity,
+    });
+    SuccessResponse.data = airplane;
+    return res.status(StatusCodes.CREATED).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+async function getAirplanes(req, res) {
+  try {
+    console.log(req);
+    const airplanes = await AirplaneService.getAirplanes();
+    SuccessResponse.data = airplanes;
+    return res.status(StatusCodes.OK).json(SuccessResponse);
+  } catch (error) {
+    ErrorResponse.error = error;
+    return res.status(error.statusCode).json(ErrorResponse);
+  }
+}
+
+module.exports = {
+  createAirplane,
+  getAirplanes,
+};
